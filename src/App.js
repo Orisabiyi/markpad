00a85f386f7712749cd539c294b1a7dcbb794380
Orisabiyi <orisabiyidavid@gmail.com>
2024-06-17 17:36:02 +0100

    included a jsx for the navbar, included it in app and styled it in index.css

--- src/App.js
+++ src/App.js
@@ -1,3 +1,9 @@
+import Navbar from "./components/Navbar";
+
 export default function App() {
-  return <h1>markPad</h1>;
+  return (
+    <>
+      <Navbar />
+    </>
+  );
 }
---
+++ src/components/Navbar.js
@@ -0,0 +1,10 @@
+export default function Navbar() {
+  return (
+    <nav className="navbar">
+      <div className="navbar-menu">
+        <img src="asset/menu.png" alt="hamburger menu icon" />
+      </div>
+      <h1>markpad</h1>
+    </nav>
+  );
+}
