1548888a79e91d3e9b0423b554e5067811549697
Orisabiyi <orisabiyidavid@gmail.com>
2024-06-17 19:57:43 +0100

    included a parser for the markdown and render it for the user, changed the styling for the editor a little bit

--- src/components/Editor.js
+++ src/components/Editor.js
@@ -1,36 +1,53 @@
 import { useState } from "react";
+import ReactMarkdown from "react-markdown";
 
 export default function Editor() {
   const [content, setContent] = useState("");
+  const [preview, setPreview] = useState(false);
 
   function handleSetContent(e) {
     setContent(e.target.value);
   }
 
-  content && console.log(content);
+  function handleSetPreview() {
+    setPreview((value) => !value);
+  }
 
   return (
     <div className="editor">
       <div className="editor--1">
         <div className="editor__heading">
           <h2>markpad</h2>
-          <button className="editor__view">
-            <img src="asset/view.png" alt="an eye icon for viewing" />
-          </button>
+          {!preview && <Button onSetPreview={handleSetPreview} />}
         </div>
         <div className="editor__area">
-          <textarea value={content} onChange={handleSetContent}></textarea>
+          <textarea
+            value={content}
+            onChange={handleSetContent}
+            spellCheck={true}
+          ></textarea>
         </div>
       </div>
 
-      <div className="editor--1">
-        <div className="editor__heading">
-          <h2>preview</h2>
+      {preview && (
+        <div className="editor--1">
+          <div className="editor__heading">
+            <h2>preview</h2>
+            {preview && <Button onSetPreview={handleSetPreview} />}
+          </div>
+          <div className="editor__area">
+            <ReactMarkdown children={content} />
+          </div>
         </div>
-        <div className="editor__area">
-          {/* <textarea value={content} onChange={handleSetContent}></textarea> */}
-        </div>
-      </div>
+      )}
     </div>
+  );
+}
+
+function Button({ onSetPreview }) {
+  return (
+    <button className="editor__view" onClick={onSetPreview}>
+      <img src="asset/view.png" alt="an eye icon for viewing" />
+    </button>
   );
 }
