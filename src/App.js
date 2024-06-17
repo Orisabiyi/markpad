658705d8fa301033448ede4a6ab834bc48faf213
Orisabiyi <orisabiyidavid@gmail.com>
2024-06-17 18:41:17 +0100

    styled the editor, included an eye icon to preview the template, include the Editor component in the App component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
+import Editor from "./components/Editor";
 import Navbar from "./components/Navbar";
 
 export default function App() {
   return (
     <>
       <Navbar />
+      <Editor />
     </>
   );
 }
