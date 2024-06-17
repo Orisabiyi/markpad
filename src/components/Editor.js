57d9b1a3af7ba6f80a68721e3b52b77c71379318
Orisabiyi <orisabiyidavid@gmail.com>
2024-06-17 19:03:23 +0100

    created the preview pane for the markdown written

--- src/components/Editor.js
+++ src/components/Editor.js
@@ -7,16 +7,29 @@ export default function Editor() {
     setContent(e.target.value);
   }
 
+  content && console.log(content);
+
   return (
     <div className="editor">
-      <div className="editor__heading">
-        <h2>markpad</h2>
-        <button className="editor__view">
-          <img src="asset/view.png" alt="an eye icon for viewing" />
-        </button>
+      <div className="editor--1">
+        <div className="editor__heading">
+          <h2>markpad</h2>
+          <button className="editor__view">
+            <img src="asset/view.png" alt="an eye icon for viewing" />
+          </button>
+        </div>
+        <div className="editor__area">
+          <textarea value={content} onChange={handleSetContent}></textarea>
+        </div>
       </div>
-      <div className="editor__area">
-        <textarea value={content} onChange={handleSetContent}></textarea>
+
+      <div className="editor--1">
+        <div className="editor__heading">
+          <h2>preview</h2>
+        </div>
+        <div className="editor__area">
+          {/* <textarea value={content} onChange={handleSetContent}></textarea> */}
+        </div>
       </div>
     </div>
   );
